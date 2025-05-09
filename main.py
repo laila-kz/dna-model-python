b5fa460691b36cef5ede8015826d573523874d20
wekt2k04 <k.tsetse0026@uca.ac.ma>
2025-05-09 11:51:55 +0100

    Adding my code below

--- main.py
+++ main.py
@@ -49,3 +49,100 @@ class pontADN:
         elif self.liaisions_h == 3:
             return f"{self.baseGauche} ≡ {self.baseDroite}"
 
+#############################
+#### WILLY'S CODE  ##########
+#############################
+
+import random
+
+class Nucleotide:
+    def __init__(self, symbol=None):
+        if symbol not in ["A", "T", "C", "G"]:
+            print('Caractere entré invalide...\nChoix aléatoire de caractère')
+            self.symbol = random.choice(["A", "T", "C", "G"])
+        else:
+            self.symbol = symbol
+        self.complement = self.getComplement()
+
+    def getComplement(self):
+        complements = {"A": "T", "T": "A", "C": "G", "G": "C"}
+        return complements.get(self.symbol, None)
+    
+    def toString(self):
+        return f"P-D-{self.symbol}"
+
+
+class PontADN:
+    def __init__(self, symbol=None):
+        if symbol not in ["A", "T", "C", "G"]:
+            print('Caractere entré invalide...\nChoix aléatoire de caractère')
+            symbol = random.choice(["A", "T", "C", "G"])
+        self.baseGauche = symbol  # define the left base
+        self.baseDroite = Nucleotide(symbol).getComplement()
+    
+    def toString(self):
+        return f"{self.baseGauche}-{self.baseDroite}"
+    
+    def nbHydrogen(self):
+        if self.baseGauche in ["A", "T"]:
+            return 2
+        elif self.baseGauche in ["C", "G"]:
+            return 3
+        return 0
+
+
+class MoleculeADN:
+    def __init__(self, nb=50):
+        self.brin = [PontADN() for _ in range(nb)]
+        
+    def getFragment(self, pos=0, leng=10):
+        # Adjust pos and leng to avoid index errors
+        fragment = []
+        for i in range(pos, min(pos + leng, len(self.brin))):
+            fragment.append(self.brin[i].toString())
+        return "\n".join(fragment)
+    
+    def firstOccurence(self, cara): #A-T, T-A
+        if cara not in ["A", "T", "C", "G"]:
+            print("ERREUR!\nLe caractère doit être soit A, T, C, ou G")
+            return
+        print(f"Recherche de la première occurence de la liaison {PontADN(cara).toString()}")
+        
+        pos = 0
+        for elm in self.brin:
+            if elm.baseGauche == cara:
+                return f"Liaison {PontADN(cara).toString()} trouvé à la position {pos+1}"
+            pos += 1
+        return f"Liaison {PontADN(cara).toString()} non trouvée !"
+    
+    def displayFragment(self, pos=0, leng=10):
+        egal = {"A": "=", "T": "=", "C": "≡", "G": "≡"}
+        for i in range(pos, min(pos + leng, len(self.brin))):
+            base = self.brin[i].baseGauche
+            print("P       P")
+            print(" \\     /")
+            print(f"  D{base}{egal[base]}{self.brin[i].baseDroite}D")
+            print(" /     \\")
+        print("P       P")
+    
+    def fusionRate(self, pos=0, leng=10):
+        at = 0
+        cg = 0
+        for i in range(pos, min(pos + leng, len(self.brin))):
+            if self.brin[i].baseGauche in ["A", "T"]:
+                at += 1
+            else:
+                cg += 1
+        # Prevent division by zero
+        return at/cg if cg != 0 else float('infini !')
+    
+    # def searchPattern(self, pattern):
+    #     def check(x):
+    #         if x not in ["A","T","C","G"] :
+    #             return f"ERREUR ! {x} n'est pas une base"
+    #     a= list(map(check,pattern))
+    #     a=len(a)
+        
+    #     for i in range(len(self.brin)):
+    #         ind=0
+    #         if self.
